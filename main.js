b6207335bfb50ec8af0e093f830b9117c4ff20c9
Najsar <pat.najsarek@gmail.com>
2019-11-15 10:36:36 +0100

    add functionality to aplication

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 1280,
     height: 800,
-    frame: true,
+    frame: false,
     icon: 'images/icon.png',
     fullscreen: false
   })
